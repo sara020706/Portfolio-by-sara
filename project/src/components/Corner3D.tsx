3550c1c9ce704fe177d105b57a096b70fba66410
sara020706 <ps2601296@gmail.com>
2026-01-17 22:15:59 +0530

    changed the 3d component

--- project/src/components/Corner3D.tsx
+++ project/src/components/Corner3D.tsx
@@ -89,70 +89,12 @@ const Corner3D: React.FC = () => {
           className="w-full h-full flex items-center justify-center"
           style={{ transformStyle: 'preserve-3d', willChange: 'transform', transition: 'transform 300ms ease' }}
         >
-          {/* 3D Fox/Cat Animal */}
+          {/* Cute emoji animal */}
           <div
-            className="relative"
+            className="text-6xl select-none cursor-pointer drop-shadow-lg"
             style={{ transform: 'translateZ(48px)' }}
           >
-            {/* Face */}
-            <div className="w-14 h-12 bg-gradient-to-b from-orange-400 to-orange-500 rounded-full relative">
-              {/* Ears */}
-              <div 
-                className="absolute -top-3 left-1 w-0 h-0"
-                style={{ 
-                  borderLeft: '8px solid transparent',
-                  borderRight: '8px solid transparent',
-                  borderBottom: '14px solid #fb923c'
-                }}
-              />
-              <div 
-                className="absolute -top-3 right-1 w-0 h-0"
-                style={{ 
-                  borderLeft: '8px solid transparent',
-                  borderRight: '8px solid transparent',
-                  borderBottom: '14px solid #fb923c'
-                }}
-              />
-              {/* Inner ears */}
-              <div 
-                className="absolute -top-1 left-2 w-0 h-0"
-                style={{ 
-                  borderLeft: '4px solid transparent',
-                  borderRight: '4px solid transparent',
-                  borderBottom: '8px solid #fda4af'
-                }}
-              />
-              <div 
-                className="absolute -top-1 right-2 w-0 h-0"
-                style={{ 
-                  borderLeft: '4px solid transparent',
-                  borderRight: '4px solid transparent',
-                  borderBottom: '8px solid #fda4af'
-                }}
-              />
-              
-              {/* Eyes */}
-              <div className="absolute top-3 left-2 w-3 h-3 bg-white rounded-full flex items-center justify-center">
-                <div className="w-2 h-2 bg-slate-800 rounded-full relative">
-                  <div className="absolute top-0 right-0 w-1 h-1 bg-white rounded-full" />
-                </div>
-              </div>
-              <div className="absolute top-3 right-2 w-3 h-3 bg-white rounded-full flex items-center justify-center">
-                <div className="w-2 h-2 bg-slate-800 rounded-full relative">
-                  <div className="absolute top-0 right-0 w-1 h-1 bg-white rounded-full" />
-                </div>
-              </div>
-              
-              {/* Nose */}
-              <div className="absolute top-6 left-1/2 -translate-x-1/2 w-2 h-1.5 bg-rose-400 rounded-full" />
-              
-              {/* Mouth */}
-              <div className="absolute top-7 left-1/2 -translate-x-1/2 w-3 h-2 border-b-2 border-l border-r border-rose-300 rounded-b-full" />
-              
-              {/* Cheeks */}
-              <div className="absolute top-5 -left-0.5 w-2 h-1.5 bg-rose-300/60 rounded-full" />
-              <div className="absolute top-5 -right-0.5 w-2 h-1.5 bg-rose-300/60 rounded-full" />
-            </div>
+            <span role="img" aria-label="dog">&#128054;</span>
           </div>
         </div>
       </div>
